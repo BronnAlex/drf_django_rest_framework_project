5b7e39a92ca317e9aeed9d85d5c2aef9a1d97770
Aleksey <lesha.bronnikov.99@mail.ru>
2025-11-15 10:43:34 +0300

    описал сериализаторы для курсов и уроков

---
+++ materials/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from materials.models import CourseModel, LessonModel
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseModel
+        fields = '__all__'
+
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LessonModel
+        fields = '__all__'
